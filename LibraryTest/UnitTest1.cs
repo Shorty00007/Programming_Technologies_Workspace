e6aae12ca6612346fbb8ccbdd9dbb40449f570de
Shorty007-OOP <118190091+Shorty007-OOP@users.noreply.github.com>
2023-05-08 21:48:33 +0200

    Added content to DataRepository
    Forgot to add content to DataRepository class, fixed the issue

--- LibraryTest/UnitTest1.cs
+++ LibraryTest/UnitTest1.cs
@@ -8,11 +8,13 @@ namespace LibraryTest
     public class LibraryServiceTests
     {
         private ILibraryService _libraryService;
+        private DataRepository _dataRepository;
 
         [SetUp]
         public void SetUp()
         {
             _libraryService = new DataService();
+            _dataRepository = new DataRepository();
         }
 
         [Test]
@@ -115,6 +117,35 @@ namespace LibraryTest
             Assert.AreEqual(book, libraryEvent.Book);
             Assert.AreEqual(librarian, libraryEvent.Librarian);
             Assert.AreEqual(DateTime.Now.Date, libraryEvent.Date.Date);
+        }
+        [Test]
+        public void TestAddRemoveGetAllBooks()
+        {
+            var shelf1 = new Shelf(1);
+            var shelf2 = new Shelf(2);
+            _dataRepository.AddShelf(shelf1);
+            _dataRepository.AddShelf(shelf2);
+
+            var book1 = new Book(1, "Book 1", "Author 1");
+            var book2 = new Book(2, "Book 2", "Author 2");
+            var book3 = new Book(3, "Book 3", "Author 3");
+
+            _dataRepository.AddBookToShelf(shelf1.Id, book1);
+            _dataRepository.AddBookToShelf(shelf1.Id, book2);
+            _dataRepository.AddBookToShelf(shelf2.Id, book3);
+
+            var allBooks = _dataRepository.GetAllBooks();
+            Assert.AreEqual(3, allBooks.Count);
+            Assert.IsTrue(allBooks.Contains(book1));
+            Assert.IsTrue(allBooks.Contains(book2));
+            Assert.IsTrue(allBooks.Contains(book3));
+
+            _dataRepository.RemoveBookFromShelf(shelf1.Id, book1.Id);
+            allBooks = _dataRepository.GetAllBooks();
+            Assert.AreEqual(2, allBooks.Count);
+            Assert.IsFalse(allBooks.Contains(book1));
+            Assert.IsTrue(allBooks.Contains(book2));
+            Assert.IsTrue(allBooks.Contains(book3));
         }
     }
 }
